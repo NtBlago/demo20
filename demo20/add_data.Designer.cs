87b58f2827cef16d49bbfcac64be9dd5e6f64394
young rudich <zovgod69@outlook.com>
2023-02-20 23:55:47 +0500

    Добавьте файлы проекта.

---
+++ demo20/add_data.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace demo20
+{
+    partial class add_dellete_data
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(add_dellete_data));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label_name = new System.Windows.Forms.Label();
+            this.logo_pic = new System.Windows.Forms.PictureBox();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(227)))), ((int)(((byte)(131)))));
+            this.panel1.Controls.Add(this.label_name);
+            this.panel1.Controls.Add(this.logo_pic);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(862, 111);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(227)))), ((int)(((byte)(131)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 525);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(862, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_name.Location = new System.Drawing.Point(153, 39);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(202, 32);
+            this.label_name.TabIndex = 3;
+            this.label_name.Text = "ООО «Рыбалка»";
+            // 
+            // logo_pic
+            // 
+            this.logo_pic.Image = ((System.Drawing.Image)(resources.GetObject("logo_pic.Image")));
+            this.logo_pic.Location = new System.Drawing.Point(0, 0);
+            this.logo_pic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.logo_pic.Name = "logo_pic";
+            this.logo_pic.Size = new System.Drawing.Size(119, 111);
+            this.logo_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo_pic.TabIndex = 4;
+            this.logo_pic.TabStop = false;
+            // 
+            // exit_button
+            // 
+            this.exit_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(140)))), ((int)(((byte)(81)))));
+            this.exit_button.Location = new System.Drawing.Point(695, 457);
+            this.exit_button.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(154, 62);
+            this.exit_button.TabIndex = 8;
+            this.exit_button.Text = "Выйти обратно.";
+            this.exit_button.UseVisualStyleBackColor = false;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // add_dellete_data
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(862, 625);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "add_dellete_data";
+            this.Text = "Добавление/Удаление данных.";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.PictureBox logo_pic;
+        private System.Windows.Forms.Button exit_button;
+    }
+}
---
+++ demo20/view_form.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace demo20
+{
+    partial class view_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(view_form));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label_name = new System.Windows.Forms.Label();
+            this.logo_pic = new System.Windows.Forms.PictureBox();
+            this.dgw_dataviewer = new System.Windows.Forms.DataGridView();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.trade3DataSet = new demo20.Trade3DataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.productTableAdapter = new demo20.Trade3DataSetTableAdapters.ProductTableAdapter();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_dataviewer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trade3DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(227)))), ((int)(((byte)(131)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 646);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 84);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(227)))), ((int)(((byte)(131)))));
+            this.panel2.Controls.Add(this.label_name);
+            this.panel2.Controls.Add(this.logo_pic);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1038, 124);
+            this.panel2.TabIndex = 1;
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_name.Location = new System.Drawing.Point(153, 44);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(202, 32);
+            this.label_name.TabIndex = 2;
+            this.label_name.Text = "ООО «Рыбалка»";
+            // 
+            // logo_pic
+            // 
+            this.logo_pic.Image = ((System.Drawing.Image)(resources.GetObject("logo_pic.Image")));
+            this.logo_pic.Location = new System.Drawing.Point(0, 4);
+            this.logo_pic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.logo_pic.Name = "logo_pic";
+            this.logo_pic.Size = new System.Drawing.Size(119, 120);
+            this.logo_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo_pic.TabIndex = 2;
+            this.logo_pic.TabStop = false;
+            // 
+            // dgw_dataviewer
+            // 
+            this.dgw_dataviewer.AutoGenerateColumns = false;
+            this.dgw_dataviewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_dataviewer.DataSource = this.trade3DataSet;
+            this.dgw_dataviewer.Location = new System.Drawing.Point(0, 131);
+            this.dgw_dataviewer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgw_dataviewer.Name = "dgw_dataviewer";
+            this.dgw_dataviewer.RowHeadersWidth = 51;
+            this.dgw_dataviewer.RowTemplate.Height = 27;
+            this.dgw_dataviewer.Size = new System.Drawing.Size(1038, 432);
+            this.dgw_dataviewer.TabIndex = 2;
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataMember = "Product";
+            this.productBindingSource.DataSource = this.trade3DataSet;
+            // 
+            // trade3DataSet
+            // 
+            this.trade3DataSet.DataSetName = "Trade3DataSet";
+            this.trade3DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(140)))), ((int)(((byte)(81)))));
+            this.button1.Location = new System.Drawing.Point(35, 582);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавление данных.";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(140)))), ((int)(((byte)(81)))));
+            this.button2.Location = new System.Drawing.Point(264, 582);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удаление данных.";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(140)))), ((int)(((byte)(81)))));
+            this.button3.Location = new System.Drawing.Point(795, 582);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // productTableAdapter
+            // 
+            this.productTableAdapter.ClearBeforeFill = true;
+            // 
+            // view_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1038, 730);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgw_dataviewer);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "view_form";
+            this.Text = "Просмотр данных.";
+            this.Load += new System.EventHandler(this.view_form_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_dataviewer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trade3DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox logo_pic;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.DataGridView dgw_dataviewer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Trade3DataSet trade3DataSet;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private Trade3DataSetTableAdapters.ProductTableAdapter productTableAdapter;
+    }
+}
